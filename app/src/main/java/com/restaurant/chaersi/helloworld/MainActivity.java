d4cd8178267aabbaa0fdcedc89aca594fb949ca8
CallMonster <chaersilee@qq.com>
2016-06-03 11:17:36 +0800

    add code

--- app/src/main/java/com/restaurant/chaersi/helloworld/MainActivity.java
+++ app/src/main/java/com/restaurant/chaersi/helloworld/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("test","Hello World2");
+        Log.i("test","Hello World3");
     }
 }
